a0a5099e5366a4132adbf5cd38dea84476735d6a
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-12 13:51:12 +0700

    feat: create validation form

---
+++ src/app/dashboard/(home)/flights/lib/validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const formFlightSchema = z.object({
+  planeId: z.string({ required_error: "Please select a plane" }),
+  price: z.string({ required_error: "Please enter a price" }),
+  departureCity: z.string({ required_error: "Please enter a departure city" }),
+  departureDate: z.date({ required_error: "Please enter a departure date" }),
+  departureCityCode: z
+    .string({
+      required_error: "Please enter a departure city code",
+    })
+    .min(3, "City code must be 3 characters long")
+    .max(3, "City code must be 3 characters long"),
+
+  destination: z.string({ required_error: "Please enter a destination" }),
+  arrivalDate: z.date({ required_error: "Please enter a departure date" }),
+  destinationCode: z
+    .string({
+      required_error: "Please enter a destination code",
+    })
+    .min(3, "City code must be 3 characters long")
+    .max(3, "City code must be 3 characters long"),
+});
